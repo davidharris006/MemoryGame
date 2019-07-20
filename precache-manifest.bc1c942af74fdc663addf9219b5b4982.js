3dcdc7cf90f0d27b4ecfff56c61af5cdd4e576e2
David Harris <davidharris006@gmail.com>
2019-07-19 21:11:34 -0700

    Updates

---
+++ precache-manifest.bc1c942af74fdc663addf9219b5b4982.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2cd687d43328dff846b74fa217a1eab3",
+    "url": "/davidharris006/MemoryGame/index.html"
+  },
+  {
+    "revision": "70d115bf427cf3e2a5a1",
+    "url": "/davidharris006/MemoryGame/static/css/main.6b7890b8.chunk.css"
+  },
+  {
+    "revision": "1c9ed9381faa4bba1ac8",
+    "url": "/davidharris006/MemoryGame/static/js/2.97433306.chunk.js"
+  },
+  {
+    "revision": "70d115bf427cf3e2a5a1",
+    "url": "/davidharris006/MemoryGame/static/js/main.236448a7.chunk.js"
+  },
+  {
+    "revision": "163a89efd5711cffc43f",
+    "url": "/davidharris006/MemoryGame/static/js/runtime~main.86835307.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/davidharris006/MemoryGame/static/media/logo.5d5d9eef.svg"
+  }
+]);
